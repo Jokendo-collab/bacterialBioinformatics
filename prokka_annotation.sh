edbed298fce61296fae9b97150a806f377a0faeb
Javan Okendo <javanokendo@gmail.com>
2023-09-28 15:20:20 -0400

    Update prokka_annotation.sh

--- prokka_annotation.sh
+++ prokka_annotation.sh
@@ -23,3 +23,16 @@ $prokka_executable \
 
 echo "Genome annotation with Prokka completed."
 
+# Functional annotation using eggNOG-mapper (optional)
+eggNOG_mapper_executable="/path/to/eggnog-mapper.py"
+eggNOG_database="/path/to/eggnog.db"
+
+$eggNOG_mapper_executable \
+    -i "$output_dir/$input_genome.ffn" \  # Prokka's predicted protein-coding genes
+    -o "$output_dir/eggnog_annotation" \
+    --database "$eggNOG_database" \
+    --cpu 4  # Number of CPU threads to use
+
+echo "Functional annotation with eggNOG-mapper completed."
+
+
